648359eaca85424aaea8c292a6e0252755b3ea1e
Léo Germond <leo.germond@gmail.com>
2017-03-30 16:26:57 +0200

    refacto for PUT

--- ws/base.py
+++ ws/base.py
@@ -48,6 +48,15 @@ class CoiotWs:
                 return v[0](psub, *v[1])
         raise SubNotFound(name, p, accept)
 
+    def route_sub_set(self, path, data, name, **accept):
+        if len(path) == 0:
+            return {'name': name, 'description': description, 'accept': list(accept.keys())}
+        p, psub = path[0], path[1:]
+        for k,v in accept.items():
+            if p == k:
+                return v[0](psub, data, *v[1])
+        raise SubNotFound(name, p, accept)
+
     def get_single_device(self, path, d):
         return self.route_sub_get(path,
                 "attribute", "attribute to get from the device",
@@ -55,16 +64,26 @@ class CoiotWs:
                 **{"*": (lambda p, d: {n: v if not callable(v) else v(d) for (n,v) in d.items() }, [d])})
 
     def set_single_device(self, path, data, d):
-        if path[1] == "*":
+        p = path[0]
+        if p == "*":
             assert(type(data) is dict)
-            assert(all((a in d.keys() for a in data.keys())))
+            assert(all(type(data[k]) is type(d[k]) for k in data.keys()))
             for k,v in data.items():
                 d[k] = v
-        elif path[1] in d.keys():
-            assert(type(data) is type(d[path[1]]))
-            d[path[1]] = data
         else:
-            raise OSError("unknown attribute: " + path[1])
+            assert(p in d)
+            assert(type(data) is type(d[p]))
+            d[p] = data
+
+    def set_multiple_devices(self, path, data, devices):
+        p = path[0]
+        assert(type(data) is dict)
+        if p == '*':
+            for d in devices.values():
+                self.set_single_device(path, data, d)
+        else:
+            for k in data.keys():
+                self.set_single_device(path, data[k], devices[k])
 
     def get_device(self, path, devices):
         return self.route_sub_get(path,
@@ -73,14 +92,9 @@ class CoiotWs:
                 **{"*": (lambda p, d: {k: self.get_single_device(p, v) for (k,v) in d.items() }, [devices])})
 
     def set_device(self, path, data, devices):
-        if path[0] in devices.keys():
-            return self.set_single_device(path, data, devices[path[0]])
-        elif path[0] == "*":
-            a={}
-            for i,s in data.items():
-                self.set_single_device(path, data, s, devices[i])
-            return a
-        raise OSError("unknown device: " + path[0])
+        return self.route_sub_set(path, data, "device",
+                **{ k: (self.set_single_device, [v]) for (k,v) in devices.items() },
+                **{"*": (self.set_multiple_devices, [devices])})
 
     def get_v1(self, path):
         return self.route_sub_get(path,
@@ -88,10 +102,8 @@ class CoiotWs:
                 device = (self.get_device, [devices]))
 
     def set_v1(self, path, data):
-        p, psub = path[0], path[1:]
-        if p == "device":
-            return self.set_device(psub, data, devices)
-        raise OSError("unknown category: " + p)
+        return self.route_sub_set(path, data, "version",
+                device = (self.set_device, [devices]))
 
     def get(self, request):
         path = [p for p in request.split("/") if p != ""]
@@ -101,7 +113,5 @@ class CoiotWs:
 
     def set(self, request, data):
         path = [p for p in request.split("/") if p != ""]
-        p, psub = path[1], path[2:]
-        if p == "v1":
-            return self.set_v1(psub, data)
-        raise OSError("unsuported protocol version: " + p)
+        return self.route_sub_set(path[1:], data, "version",
+                v1 = (self.set_v1, []))
